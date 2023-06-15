e84fccfee8eb9a8ad1c07266093497c0f83649bb
Rutuja Asabe <97525707+Rutujaasabe@users.noreply.github.com>
2023-06-16 00:06:32 +0530

    Add files via upload

---
+++ Collection_Framework.java
@@ -0,0 +1,12 @@
+package com.acc;
+import java.util.ArrayList;
+public class Collection_Framework {
+	public static void main(String rt[]) {
+		ArrayList<Float> value =new ArrayList<Float>();
+		value.add(1.2f);
+		value.add(2.0f);
+		value.add(2.9f);
+		value.add(3.0f);
+		System.out.println(value);
+	}
+}
